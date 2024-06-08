24216d00fac46b2656302f82b6b73bb4738cb10d
ricardobar96 <ricardobar96@gmail.com>
2024-06-08 16:16:20 +0100

    Form view linked to main view via url

--- django_app/base/urls.py
+++ django_app/base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ListToDo, DetailTask
+from .views import ListToDo, DetailTask, CreateTask
 
 urlpatterns = [path('', ListToDo.as_view(), name='toDo'),
-               path('task/<int:pk>', DetailTask.as_view(), name='task')]
+               path('task/<int:pk>', DetailTask.as_view(), name='task'),
+               path('create-task/', CreateTask.as_view(), name='create-task')]
--- django_app/base/views.py
+++ django_app/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 class  ListToDo(ListView):
@@ -10,3 +12,8 @@ class  ListToDo(ListView):
 class  DetailTask(DetailView):
     model = Task
     context_object_name = 'task'
+
+class CreateTask(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
