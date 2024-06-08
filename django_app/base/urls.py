f59dc6f613827d543255f9ef466e2ae7b0dbd13d
ricardobar96 <ricardobar96@gmail.com>
2024-06-08 16:00:28 +0100

    Added view with task's details, linked to main view by urls

--- django_app/base/urls.py
+++ django_app/base/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from .views import ListToDo
+from .views import ListToDo, DetailTask
 
-urlpatterns = [path('', ListToDo.as_view(), name='To Do')]
+urlpatterns = [path('', ListToDo.as_view(), name='toDo'),
+               path('task/<int:pk>', DetailTask.as_view(), name='task')]
--- django_app/base/views.py
+++ django_app/base/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 class  ListToDo(ListView):
     model = Task
     context_object_name = 'tasks'
+
+class  DetailTask(DetailView):
+    model = Task
+    context_object_name = 'task'
