35b3fdc4a5bf5428d2772c79da2980337f2e68f5
ricardobar96 <ricardobar96@gmail.com>
2024-06-08 16:58:31 +0100

    Edit form functional and linked to tasks that can be updated

--- django_app/base/urls.py
+++ django_app/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListToDo, DetailTask, CreateTask
+from .views import ListToDo, DetailTask, CreateTask, UpdateTask
 
 urlpatterns = [path('', ListToDo.as_view(), name='tasks'),
                path('task/<int:pk>', DetailTask.as_view(), name='task'),
-               path('create-task/', CreateTask.as_view(), name='create-task')]
+               path('create-task/', CreateTask.as_view(), name='create-task'),
+               path('update-task/<int:pk>', UpdateTask.as_view(), name='update-task')]
