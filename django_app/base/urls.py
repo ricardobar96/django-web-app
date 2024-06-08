07335069e98c14dfeecdcc6429f367661e6f1433
ricardobar96 <ricardobar96@gmail.com>
2024-06-08 18:23:40 +0100

    Functional login and logout user

--- django_app/base/urls.py
+++ django_app/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ListToDo, DetailTask, CreateTask, UpdateTask, DeleteTask
+from .views import ListToDo, DetailTask, CreateTask, UpdateTask, DeleteTask, Login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [path('', ListToDo.as_view(), name='tasks'),
+               path('login/', Login.as_view(), name='login'),
+               path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
                path('task/<int:pk>', DetailTask.as_view(), name='task'),
                path('create-task/', CreateTask.as_view(), name='create-task'),
                path('update-task/<int:pk>', UpdateTask.as_view(), name='update-task'),
-               path('delete-task/<int:pk>', DeleteTask.as_view(), name='delete-task')]
+               path('delete-task/<int:pk>', DeleteTask.as_view(), name='delete-task'),]
--- django_app/base/views.py
+++ django_app/base/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from .models import Task
+
+
+class Login(LoginView):
+    template_name = "base/login.html"
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class  ListToDo(ListView):
     model = Task
