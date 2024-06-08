0e938b5a2ea4dfc6fb63e8ac6fcad8dbb45d3ae1
ricardobar96 <ricardobar96@gmail.com>
2024-06-08 19:10:24 +0100

    Functionality create new account, redirect when user exists

--- django_app/base/urls.py
+++ django_app/base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListToDo, DetailTask, CreateTask, UpdateTask, DeleteTask, Login
+from .views import ListToDo, DetailTask, CreateTask, UpdateTask, DeleteTask, Login, SignUp
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [path('', ListToDo.as_view(), name='tasks'),
                path('login/', Login.as_view(), name='login'),
                path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+               path('signUp/', SignUp.as_view(), name='signUp'),
                path('task/<int:pk>', DetailTask.as_view(), name='task'),
                path('create-task/', CreateTask.as_view(), name='create-task'),
                path('update-task/<int:pk>', UpdateTask.as_view(), name='update-task'),
--- django_app/base/views.py
+++ django_app/base/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
@@ -15,6 +17,23 @@ class Login(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+class SignUp(FormView):
+    template_name = 'base/sign_up.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(SignUp, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(SignUp, self).get(*args, **kwargs)
 
 class  ListToDo(LoginRequiredMixin, ListView):
     model = Task
